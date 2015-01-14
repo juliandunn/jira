ed9a0ade362fcca2e39e3be638b173bb53eaa63f
Julian C. Dunn <jdunn@getchef.com>
2015-01-14 15:58:13 -0500

    Added simple recipe to open the JIRA port

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'jira'
 maintainer 'Julian C. Dunn'
-maintainer_email 'jdunn@getchef.com'
+maintainer_email 'jdunn@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures jira'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
-%w(database java mysql openssl postgresql).each do |d|
+%w(database iptables java mysql openssl postgresql).each do |d|
   depends d
 end
 
